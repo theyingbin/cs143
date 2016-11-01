25326e28b4c63f308eaecfbab9d198fd06805245
ckenny9739 <ckenny9739@ucla.edu>
2016-10-31 22:25:40 -0700

    Finished rough draft of movie info

--- project1c/add_movie_information.php
+++ project1c/add_movie_information.php
@@ -11,7 +11,6 @@
 </head>
 
 <body>
-
   <ul>
     <li><a href="index.php">Home</a></li>
     <li class="dropdown">
@@ -52,7 +51,7 @@
         <label for="year">Year</label>
         <input type="text" class="form-control" placeholder="Input a Year (ex. 2008)" name="year"/>
       </div>
-      <div>
+      <div class="form-group">
         <label for="rating">MPAA Rating</label>
       	<select name="rating">
     	  <option value="G">G</option>
@@ -62,54 +61,76 @@
     	  <option value="NC-17">NC-17</option>
   	  	</select>
       </div>
-      <div>
-        <label for="genre">Genre</label>
-      	<select name="genre">
-    	  <option value="Action">Action</option>
-    	  <option value="Adventure">Adventure</option>
-    	  <option value="Animation">Animation</option>
-    	  <option value="Biography">Biography</option>
-    	  <option value="Comedy">Comedy</option>
-    	  <option value="Crime">Crime</option>
-    	  <option value="Documentary">Documentary</option>
-    	  <option value="Drama">Drama</option>
-    	  <option value="Family">Family</option>
-    	  <option value="Fantasy">Fantasy</option>
-    	  <option value="Film-Noir">Film-Noir</option>
-    	  <option value="History">History</option>
-    	  <option value="Horror">Horror</option>
-    	  <option value="Music">Music</option>
-    	  <option value="Musical">Musical</option>
-    	  <option value="Mystery">Mystery</option>
-    	  <option value="Romance">Romance</option>
-    	  <option value="Sci-Fi">Sci-Fi</option>
-    	  <option value="Sport">Sport</option>
-    	  <option value="Thriller">Thriller</option>
-    	  <option value="War">War</option>
-    	  <option value="Western">Western</option>
-  	  	</select>
+      <div class="form-group">
+        <label for="genre">Genre:</label>
+					<input type="checkbox" name="genre[]" value="Action">Action</input>
+					<input type="checkbox" name="genre[]" value="Adult">Adult</input>
+					<input type="checkbox" name="genre[]" value="Adventure">Adventure</input>
+					<input type="checkbox" name="genre[]" value="Animation">Animation</input>
+					<input type="checkbox" name="genre[]" value="Comedy">Comedy</input>
+					<input type="checkbox" name="genre[]" value="Crime">Crime</input>
+					<input type="checkbox" name="genre[]" value="Documentary">Documentary</input>
+					<input type="checkbox" name="genre[]" value="Drama">Drama</input>
+					<input type="checkbox" name="genre[]" value="Family">Family</input>
+					<input type="checkbox" name="genre[]" value="Fantasy">Fantasy</input>
+					<br></br>
+					<input type="checkbox" name="genre[]" value="Horror">Horror</input>
+					<input type="checkbox" name="genre[]" value="Musical">Musical</input>
+					<input type="checkbox" name="genre[]" value="Mystery">Mystery</input>
+					<input type="checkbox" name="genre[]" value="Romance">Romance</input>
+					<input type="checkbox" name="genre[]" value="Sci-Fi">Sci-Fi</input>				
+					<input type="checkbox" name="genre[]" value="Short">Short</input>
+					<input type="checkbox" name="genre[]" value="Thriller">Thriller</input>
+					<input type="checkbox" name="genre[]" value="War">War</input>
+					<input type="checkbox" name="genre[]" value="Western">Western</input>
+					<br></br>
       </div>
-      <button type="submit" name="submit" class="btn btn-default">Add!</button>
+      <button type="submit" name="submit" class="btn btn-default">Add Movie Information!</button>
     </form>
 <?php
-  $title=$_GET["title"];
-  $company=$_GET["company"];
+	$db = new mysqli('localhost', 'cs143', '', 'CS143');
+	if($db->connect_errno > 0){
+    	die('Unable to connect to database [' . $db->connect_error . ']');
+	}
+
+  $title=trim($_GET["title"]);
+  $company=trim($_GET["company"]);
   $year=$_GET["year"];
   $rating=$_GET["rating"];
   $genre=$_GET["genre"];
 
-  if(isset($_GET['submit'])){
-    echo "helloworld<br>";
-    if(strlen($title) == 0 || strlen($company) == 0 || strlen($year) == 0){
-      echo "<h4>Invalid Input. A field was left empty</h4>";
-      exit(1);
-    }
-    print_r($title);
-    print_r($company);
-    print_r($year);
-    print_r($rating);
-    print_r($genre);
-  }
+	if ($title == '' && $company == '' && $year == '' && $rating == '' && count($genre) == 0) {
+		// Do Nothing - no query yet
+	} else if ($title == '') {
+		echo "Please enter a valid title.";
+	} else if ($company == '') {
+		echo "Please enter a valid company.";
+	} else if ($year == '' || $year <= 1500 || $year >= 2100) {
+		echo "Please enter a valid year.";	
+	} else { // Valid input
+		$maxIDs = $db->query("SELECT MAX(id) FROM MaxMovieID") or die(mysqli_error($db));
+
+		$maxIDArr = mysqli_fetch_array($maxIDs);
+		$curMaxID = $maxIDArr[0];
+		$newMaxID = $curMaxID + 1;
+
+		$company = $db->real_escape_string($company);
+		$title = $db->real_escape_string($title);
+
+		$MIquery = $db->query("INSERT INTO Movie (id, title, year, rating, company) VALUES ('$newMaxID', '$title', '$year', '$rating', '$company')") or die(mysqli_error($db));
+
+		$db->query("UPDATE MaxMovieID SET id=$newMaxID WHERE id=$curMaxID") or die(mysqli_error($db));
+
+		for ($i = 0; $i < count($genre); $i++) {
+			$Gquery = $db->query("INSERT INTO MovieGenre (mid, genre) VALUES ('$newMaxID', '$genre[$i]')") or die(mysqli_error($db));
+		}
+
+		echo $title . " Added!";
+	}
+  $maxIDr->free();
+  $MIquery->free();
+  $Gquery->free();
+  $db->close();
 ?>
   </div>
 </body>
