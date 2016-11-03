8e1ffd73011d8bb94d847d6f04138f10ea8770f3
ckenny9739 <ckenny9739@ucla.edu>
2016-11-02 20:39:02 -0700

    fixed typo

--- project1c/index.php
+++ project1c/index.php
@@ -7,7 +7,7 @@
   <div class="jumbotron" align="center">
 	<h1 align="center">Welcome to Project 1C</h1>
 	<h2 align="center">Created by Connor Kenny and Ying Bin Wu</h2>
-    This is a Movie Database for CS 143 taught by Professor Cho. It is supposed to mimic the basic idea behind IMDB. You can add Actors, Movies, and Relations, while also being able to search for information regarding specific Movies and Actors! Feel free to play around with the site and have fun!
+    This is a Movie Database Interface for CS 143 taught by Professor Cho. It is supposed to mimic the basic idea behind IMDB. You can add Actors, Movies, and Relations, while also being able to search for information regarding specific Movies and Actors! Feel free to play around with the site and have fun!
   </div>
   </div>
 </body>
