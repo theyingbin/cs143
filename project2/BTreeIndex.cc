5454b3f269a1cc2b7d1c8d716da4a4b65632578a
ckenny9739 <ckenny9739@ucla.edu>
2016-11-14 23:57:33 -0800

    starting 2C

--- project2/BTreeIndex.cc
+++ project2/BTreeIndex.cc
@@ -18,6 +18,8 @@ using namespace std;
 BTreeIndex::BTreeIndex()
 {
     rootPid = -1;
+    treeHeight = 0;
+    memset(buffer, 0, PageFile::PAGE_SIZE);
 }
 
 /*
@@ -29,6 +31,62 @@ BTreeIndex::BTreeIndex()
  */
 RC BTreeIndex::open(const string& indexname, char mode)
 {
+	RC errorCode = pf.open(indexname, mode);
+	
+	if (errorCode != 0)
+		return errorCode;
+
+	/*
+
+	I don't think this section is needed
+
+	// Check that indexname is opened for the first time
+	// Do we even need this?
+	if (pf.endPid() == 0) {
+		rootPid = -1;
+		treeHeight = 0;
+
+		// check that it is opened for writing if first time
+		// I don't think this is needed...
+		// 
+		// errorCode = pf.write(0, buffer);
+		// if(errorCode != 0)
+		// 	return errorCode;
+
+		return 0;
+	}
+
+	*/
+
+
+	// Does it make sense to store rootPid and treeHeight in the 0th Disk Page?
+
+	errorCode = pf.read(0, buffer);
+
+	if (errorCode != 0)
+		return errorCode;
+
+	// Is this use of temps to validate the values necessary?
+	// I feel like if we store this info on the 0th disk page, then there could be garbage there on initial open
+	// So maybe handle that up at the pf.endPid() == 0 check?
+
+	PageId tempRootPid;
+	int tempTreeHeight;
+
+  	memcpy(buffer, &tempRootPid, sizeof(PageId));
+	memcpy(buffer + sizeof(PageId), &tempTreeHeight, sizeof(int));
+
+	// Can't be 0 bc 0 is holding rootPid, treeHeight
+	if (tempRootPid > 0 && tempTreeHeight >= 0) {
+		rootPid = tempRootPid;
+		treeHeight = tempTreeHeight;
+	}
+
+	/*
+	memcpy(buffer, &rootPid, sizeof(PageId));
+	memcpy(buffer + sizeof(PageId), &treeHeight, sizeof(int));
+	*/
+
     return 0;
 }
 
@@ -38,7 +96,15 @@ RC BTreeIndex::open(const string& indexname, char mode)
  */
 RC BTreeIndex::close()
 {
-    return 0;
+	memcpy(buffer, &rootPid, sizeof(PageId));
+	memcpy(buffer + sizeof(PageId), &treeHeight, sizeof(int));
+
+	RC errorCode = pf.write(0, buffer);
+
+	if (errorCode != 0)
+		return errorCode;
+
+    return pf.close();
 }
 
 /*
@@ -49,6 +115,7 @@ RC BTreeIndex::close()
  */
 RC BTreeIndex::insert(int key, const RecordId& rid)
 {
+	// Probably do this recursively to keep track of parent too?
     return 0;
 }
 
@@ -72,7 +139,41 @@ RC BTreeIndex::insert(int key, const RecordId& rid)
  */
 RC BTreeIndex::locate(int searchKey, IndexCursor& cursor)
 {
-    return 0;
+	RC errorCode;	
+	BTNonLeafNode nonLeafNode;
+	BTLeafNode leafNode;
+	int height = 1;
+	PageId pid = rootPid;
+	int eid;
+
+	while (height != treeHeight) {
+		// Get the non leaf node content
+		errorCode = nonLeafNode.read(pid, pf);
+		if (errorCode != 0)
+			return errorCode;
+
+		// Get the child pointer we want to follow
+		errorCode = nonLeafNode.locateChildPtr(searchKey, pid);
+		if (errorCode != 0)
+			return errorCode;
+
+		height++;
+	}
+
+	// Get the leafnode content
+	errorCode = leafNode.read(pid, pf);
+	if (errorCode != 0)
+		return errorCode;
+
+	// Look for the searchKey
+	// If no such record, errorcode is set to RC_NO_SUCH_RECORD
+	// And eid is set correctly to just after largest element that is smaller (aka right place)
+	// so just assign and return either way
+	errorCode = leafNode.locate(searchKey, eid);
+	cursor.pid = pid;
+	cursor.eid = eid;
+
+    return errorCode;
 }
 
 /*
@@ -85,5 +186,36 @@ RC BTreeIndex::locate(int searchKey, IndexCursor& cursor)
  */
 RC BTreeIndex::readForward(IndexCursor& cursor, int& key, RecordId& rid)
 {
+	RC errorCode;
+
+	PageId cursorPid = cursor.pid;
+	int cursorEid = cursor.eid;
+
+	// 0 not allowed bc store rootPid and treeHeight there
+	if(cursorPid <= 0 || cursorEid < 0)
+		return RC_INVALID_CURSOR;
+
+	BTLeafNode leafNode;
+	// Get leaf node content
+	errorCode = leafNode.read(cursorPid, pf);
+	if (errorCode != 0) 
+		return errorCode;
+
+	// Get entry info
+	errorCode = leafNode.readEntry(cursorEid, key, rid);
+	if (errorCode != 0) 
+		return errorCode;
+
+	// Make sure to account for overflow
+	if (cursorEid + 1 >= leafNode.getKeyCount()) {
+		cursorEid = 0;
+		cursorPid = leafNode.getNextNodePtr();
+	} else {
+		cursorEid++;
+	}
+
+	cursor.eid = cursorEid;
+	cursor.pid = cursorPid;
+
     return 0;
 }
--- project2/BTreeIndex.h
+++ project2/BTreeIndex.h
@@ -97,6 +97,9 @@ class BTreeIndex {
   /// this class is destructed. Make sure to store the values of the two 
   /// variables in disk, so that they can be reconstructed when the index
   /// is opened again later.
+
+  char buffer[PageFile::PAGE_SIZE];
+
 };
 
 #endif /* BTREEINDEX_H */
